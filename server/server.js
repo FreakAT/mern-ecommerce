faa7d6d64203d36a731f566ad08743817aa89a09
Hariom <hariom12062000@gmail.com>
2024-09-30 23:04:44 +0530

    changes for vercel

--- server/server.js
+++ server/server.js
@@ -23,11 +23,11 @@ mongoose
 	.catch((error) => console.log(error));
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: process.env.CLIENT_URL,
 		methods: ["GET", "POST", "DELETE", "PUT"],
 		allowedHeaders: [
 			"Content-Type",
