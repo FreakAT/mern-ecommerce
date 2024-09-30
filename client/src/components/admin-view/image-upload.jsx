3a82e37e062cef7be24048491eaf515c6631df9e
Hariom <hariom12062000@gmail.com>
2024-09-30 23:10:29 +0530

    changes for vercel

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -46,7 +46,7 @@ function ProductImageUpload({
 		const data = new FormData();
 		data.append("my_file", imageFile);
 		const response = await axios.post(
-			"http://localhost:5000/api/admin/products/upload-image",
+			"https://mern-ecommerce-backend-lake.vercel.app/api/admin/products/upload-image",
 			data
 		);
 
